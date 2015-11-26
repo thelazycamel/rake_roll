ee5f8c66a3cffa4652c715ee28df46a6e8174eaf
Stuart Hanscombe <hanscs1969@yahoo.co.uk>
2015-11-26 15:02:33 +0100

    updating to 0.2.4

--- lib/rake_roll/railtie.rb
+++ lib/rake_roll/railtie.rb
@@ -6,7 +6,7 @@ module RakeRoll
     railtie_name :rake_roll
 
     rake_tasks do
-      import "rake_roll/tasks/roll.rake"
+      import File.join(File.dirname(__FILE__), "rake_roll/tasks/roll.rake")
     end
   end
 
